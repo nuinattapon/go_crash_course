cd4b4976fb2c4b5cc0add4ba8f22881c0966046c
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2021-04-27 10:30:22 +0700

    Update 16_cli to use 	"github.com/urfave/cli/v2"

--- 16_cli/cli.go
+++ 16_cli/cli.go
@@ -6,90 +6,92 @@ import (
 	"net"
 	"os"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "Website Lookup CLI"
-	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
+	app.Usage = "Let's you query IPs, CNAMs, MX records and Name Servers"
 
-	// We'll be using the same flag for all our commands
-	// so we'll define it up here
 	myFlags := []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "host",
-			Value: "nattapon.me",
+			Value: "oracle.com",
 		},
 	}
 
-	// we create our commands
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
 			Name:  "ns",
-			Usage: "Looks Up the NameServers for a Particular Host",
+			Usage: "Looks up the Name Servers for a Particular Host",
 			Flags: myFlags,
-			// the action, or code that will be executed when
-			// we execute our `ns` command
 			Action: func(c *cli.Context) error {
-				// a simple lookup function
-				ns, err := net.LookupNS(c.String("url"))
+				ns, err := net.LookupNS(c.String("host"))
 				if err != nil {
+					fmt.Println(err)
 					return err
 				}
-				// we log the results to our console
-				// using a trusty fmt.Println statement
 				for i := 0; i < len(ns); i++ {
 					fmt.Println(ns[i].Host)
 				}
 				return nil
 			},
-		}, {
+		},
+		{
 			Name:  "ip",
-			Usage: "Looks up the IP addresses for a particular host",
+			Usage: "Looks Up the IP addresses for a particular host",
 			Flags: myFlags,
 			Action: func(c *cli.Context) error {
 				ip, err := net.LookupIP(c.String("host"))
 				if err != nil {
 					fmt.Println(err)
+					return err
 				}
 				for i := 0; i < len(ip); i++ {
 					fmt.Println(ip[i])
+
 				}
 				return nil
 			},
-		}, {
+		},
+		{
 			Name:  "cname",
-			Usage: "Looks up the CNAME for a particular host",
+			Usage: "Looks Up CNAME for a particular Host",
 			Flags: myFlags,
 			Action: func(c *cli.Context) error {
 				cname, err := net.LookupCNAME(c.String("host"))
+
 				if err != nil {
 					fmt.Println(err)
+					return err
 				}
 				fmt.Println(cname)
 				return nil
 			},
-		}, {
+		},
+		{
 			Name:  "mx",
-			Usage: "Looks up the MX records for a particular host",
+			Usage: "Look Up for mx records for a particular Host",
 			Flags: myFlags,
 			Action: func(c *cli.Context) error {
 				mx, err := net.LookupMX(c.String("host"))
+
 				if err != nil {
 					fmt.Println(err)
+					return err
 				}
 				for i := 0; i < len(mx); i++ {
 					fmt.Println(mx[i].Host, mx[i].Pref)
 				}
 				return nil
+
 			},
 		},
 	}
-
-	// start our application
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
+
 	}
 }
--- 16_cli/go.mod
+++ 16_cli/go.mod
@@ -2,4 +2,7 @@ module github.com/nuinattapon/go_crash_course/16_cli
 
 go 1.16
 
-require github.com/urfave/cli v1.22.5
+require (
+	github.com/urfave/cli v1.22.5
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
